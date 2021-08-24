c70ca5a431540fca01fab92090010f4d304c3c41
PHILLyaHI <87790235+PHILLyaHI@users.noreply.github.com>
2021-08-24 11:42:20 -0700

    Add files via upload

---
+++ moduleE2_homework/8/index.js
@@ -0,0 +1,15 @@
+let myMap = new Map();
+
+myMap.set("FILIPP", "Derman", );
+myMap.set("KIRILL", "Golovochenko", );
+myMap.set("PASHA", "Brui", );
+
+for (let X of myMap.keys()) {
+    console.log(X);
+}
+
+for (let Y of myMap.values()) {
+    console.log(Y);
+}
+
+console.log(myMap)
